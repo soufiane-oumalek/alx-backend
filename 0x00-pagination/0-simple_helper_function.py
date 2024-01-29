b1218b4b80e43492e197f5c8cd23c1e355a9bf7b
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-01-29 21:13:40 +0100

    pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -7,4 +7,4 @@ def index_range(page, page_size):
     a start index and an end index corresponding
     to the range of indexes to return in a list for
     those particular pagination parameters """
-    return ((page -1) * page_size), ((page -1) * page_size) + page_size
+    return ((page - 1) * page_size), ((page - 1) * page_size) + page_size
