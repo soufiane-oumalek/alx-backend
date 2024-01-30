159c2f66037bc53271979a5d6577b58927bccc09
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-01-30 02:51:09 +0100

    pagination

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -41,29 +41,17 @@ class Server:
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """Return a dictionary containing pagination information."""
 
-        data = self.indexed_dataset()
+    assert isinstance(index, int) and isinstance(page_size, int)
+    assert 0 <= index < len(self.indexed_dataset())
 
-        assert index is not None and 0 <= index <= max(data.keys())
+    pages = []
+    next_index = index + page_size
+    i = index
 
-        page_data = []
-        data_count = 0
-        next_index = None
-        start = index if index else 0
-
-        for i, item in data.items():
-            if i >= start and data_count < page_size:
-                page_data.append(item)
-                data_count += 1
-                continue
-            if data_count == page_size:
-                next_index = i
-                break
-
-        pages = {
-            'index': index,
-            'next_index': next_index,
-            'page_size': len(page_data),
-            'data': page_data,
-        }
-
-        return pages
+    while len(pages) < page_size:
+        if i in self.indexed_dataset():
+            pages.append(self.indexed_dataset()[i])
+        i += 1
+        next_index += 1
+    return {'index': index, 'next_index': next_index,
+            'page_size': page_size, 'data': pages}
