eea88f82a3615c4a6b4097ebe92107abafaa2097
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-02-01 01:37:38 +0100

    caching

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
-"""2. LIFO Caching
+"""4. MRU Caching
 """
 from base_caching import BaseCaching
 
 
-class LIFOCache(BaseCaching):
-    """ Class LIFOCache.
+class MRUCache(BaseCaching):
+    """Class MRUCache.
     """
 
     def __init__(self):
         """Initializes the cache.
         """
         super().__init__()
-        self.stack = []
+        self.access_order = []
 
     def put(self, key, item):
         """ assign to the dictionary self.
@@ -22,17 +22,23 @@ class LIFOCache(BaseCaching):
         if key is None or item is None:
             return
 
-        if len(self.cache_data) >= self.MAX_ITEMS:
-            disc_key = self.stack.pop()
-            del self.cache_data[disc_key]
-            print(f"DISCARD: {disc_key}")
+        if key in self.cache_data:
+            self.access_order.remove(key)
+        elif len(self.cache_data) >= self.MAX_ITEMS:
+            discarded_key = self.access_order.pop()
+            del self.cache_data[discarded_key]
+            print(f"DISCARD: {discarded_key}")
 
         self.cache_data[key] = item
-        self.stack.append(key)
+        self.access_order.append(key)
 
     def get(self, key):
         """ return the value in self.cache_data linked to key.
         If key is None or if the key doesn’t exist in self.
         cache_data, return None.
         """
-        return self.cache_data.get(key)
+        if key in self.cache_data:
+            self.access_order.remove(key)
+            self.access_order.append(key)
+            return self.cache_data[key]
+        return None
