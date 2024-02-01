684b11516c003c15ea31538cff92b44158fac2c6
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-02-01 01:19:21 +0100

    caching

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""2. LIFO Caching
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ Class LIFOCache.
+    """
+
+    def __init__(self):
+        """Initializes the cache.
+        """
+        super().__init__()
+        self.stack = []
+
+    def put(self, key, item):
+        """ assign to the dictionary self.
+        cache_data the item value for the key key.
+        If key or item is None, this method should not do anything.
+        """
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            discarded_key = self.stack.pop()
+            del self.cache_data[discarded_key]
+            print(f"DISCARD: {discarded_key}")
+
+        self.cache_data[key] = item
+        self.stack.append(key)
+
+    def get(self, key):
+        """Retrieves an item by key.
+
+        Args:
+            key: The key of the item to retrieve.
+
+        Returns:
+            The item if found, None otherwise.
+        """
+        return self.cache_data.get(key)
