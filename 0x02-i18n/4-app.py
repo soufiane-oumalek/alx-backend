8970f1775d431ff7eed7fb5afc3a0fc724c6d382
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-02-07 00:33:42 +0100

    tasks_4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Basic Babel setup """
 from flask import Flask, render_template, request
-from flask_babel import Babel, gettext
+from flask_babel import Babel,
 
 
 class Config(object):
