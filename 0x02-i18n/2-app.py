25a89e081fb8cb52fe8869c5624a5684774610d9
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-02-06 21:32:53 +0100

    task_1

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" Basic Babel setup """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config(object):
+    """ Class Config Babel """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """ determine the best match with our supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def home() -> str:
+    """ home page """
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
