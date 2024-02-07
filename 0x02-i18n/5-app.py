e62c2fc2eb86f448a85648a2d9a10aac54ca979e
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-02-07 22:34:22 +0100

    tasks_*

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ Basic Babel setup """
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 from flask_babel import Babel
+from typing import Union, Dict
 
 
 users = {
