91988119d678f65b56aef935efae467e777e36c5
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-02-07 01:44:18 +0100

    tasks_*

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -24,7 +24,7 @@ def get_locale():
      locale argument and ifs value is a supported locale
     """
     locale = request.args.get('locale')
-    if locale:
+    if locale and locale in app.config['LANGUAGES']:
         return locale
 
     return request.accept_languages.best_match(app.config['LANGUAGES'])
