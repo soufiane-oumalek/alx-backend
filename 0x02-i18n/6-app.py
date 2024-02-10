35e02b92a9ecfa1a6c00bfc25b6b23914b9aae0a
soufiane-oumalek <soufian.oumalek@gmail.com>
2024-02-10 01:25:49 +0100

    task_*

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+""" Use user locale
+"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel, gettext
+
+app = Flask(__name__)
+babel = Babel(app)
+"""Babel object """
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config(object):
+    """ config class """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+""" class as config for Flask app """
+
+
+@app.route('/')
+def home() -> str:
+    """ home page """
+    return render_template('6-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """ the best match with our supported languages """
+    localLang = request.args.get('locale')
+    supportLang = app.config['LANGUAGES']
+    if localLang in supportLang:
+        return localLang
+    userId = request.args.get('login_as')
+    if userId:
+        localLang = users[int(userId)]['locale']
+        if localLang in supportLang:
+            return localLang
+    localLang = request.headers.get('locale')
+    if localLang in supportLang:
+        return localLang
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+def get_user():
+    """ returns a user dictionary or None
+    """
+    try:
+        userId = request.args.get('login_as')
+        return users[int(userId)]
+    except Exception:
+        return None
+
+
+@app.before_request
+def before_request():
+    """ use get_user to find a user if """
+    g.user = get_user()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000", debug=True)
